7c11e255cbbca13ccf1ab181c413afede6c51cb5
Anisa Fauziah <anisa.fauziah.ft21@mail.umy.ac.id>
2022-12-12 17:21:24 +0700

    set the FRONT position to the first element of the array

--- Queues_041/Program.cs
+++ Queues_041/Program.cs
@@ -94,6 +94,17 @@ namespace Queues_041
                 }
                 Console.WriteLine();
             }
+            else
+            {
+                /* traverses the queue till the last position of the array. */
+                while (FRONT_position <= max -1)
+                {
+                    Console.Write(queue_array[FRONT_position] + "  ");
+                    FRONT_position++;
+                }
+                /* set the FRONT position to the first element of the array. */
+                FRONT_position = 0;
+            }
         }
         static void Main(string[] args)
         {
